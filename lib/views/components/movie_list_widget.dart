74afdc71867976460780bc1a26258cecd18fe026
Kevin Zazzero <kevin.zazzero@hotmail.it>
2026-07-06 23:25:15 +0200

    completato Selfwork Edit e Delete

--- lib/views/components/movie_list_widget.dart
+++ lib/views/components/movie_list_widget.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:selfwork_sqlite/viewmodels/movie_view_model.dart';
 import 'package:provider/provider.dart';
+import 'package:selfwork_sqlite/views/components/movie_form_dialog.dart';
 
 class MovieListWidget extends StatefulWidget {
   const MovieListWidget({super.key});
@@ -26,6 +27,21 @@ class _MyWidgetState extends State<MovieListWidget> {
               child: ListTile(
                 title: Text(movie.title),
                 subtitle: Text("Anno: ${movie.year} - Durata : ${movie.duration} \n Trama: ${movie.plot}"),
+                trailing: Wrap(
+                  spacing: 8,
+                  children: [
+                    IconButton(onPressed: (){
+                      showDialog(context: context, 
+                      builder: (_) => MovieFormDialog(movie: movie,)
+                      );
+                    }, 
+                    icon: Icon(Icons.edit, color: Colors.blue)),
+                    IconButton(onPressed: (){
+                      context.read<MovieViewModel>().deleteMovie(movie.id!);
+                    }, 
+                    icon: const Icon(Icons.delete, color: Colors.red,))
+                  ],
+                ),
               ),
             );
           } ))
